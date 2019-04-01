53f3f160ae4c966581c412548d803296faa4dd52
Kenny Wu <wu.kenny1221@gmail.com>
2019-04-01 15:16:19 -0700

    Reorganize file directory + Add S3 Bucket for remote state storage

---
+++ remote_state_storage/main.tf
@@ -0,0 +1,15 @@
+provider "aws" {
+  region = "ca-central-1"
+}
+
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "terraform-up-and-running-state-kenny"
+
+  versioning {
+    enabled = true
+  }
+  
+  lifecycle {
+    prevent_destroy = true
+  }
+}
